818204640ff53dbb03e8075add399acb6ce4ec6a
puriketu99 <puriketu.white@gmail.com>
2012-09-03 20:32:09 +0900

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minch::Application.config.secret_token = 'f35a23c6c05feeb6578d1c4613053674d28fb48631b8cca01f4005acea19a25a08fee7bb11b2e31f73b5e165ecd02cf28b5b14cc2f21c49381f4698ddb352634'
